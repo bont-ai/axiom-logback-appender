a64dc897331361c4c1aa6818d4b6f3b8efdba35a
Nik Peric <nik@nikperic.com>
2025-07-18 14:31:14 -0400

    chore: deployment to clojars

--- build.clj
+++ build.clj
@@ -1,5 +1,6 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'ai.bont/axiom-logback-appender)
 (def version "1.0.0")
@@ -11,23 +12,29 @@
       (b/delete {:path "target"}))
 
 (defn compile-clj [_]
-      (b/compile-clj {:basis basis
-                      :src-dirs ["src"]
-                      :class-dir class-dir
+      (b/compile-clj {:basis        basis
+                      :src-dirs     ["src"]
+                      :class-dir    class-dir
                       :compile-opts {:direct-linking true}
-                      :ns-compile '[ai.bont.axiom-logback-appender]}))
+                      :ns-compile   '[ai.bont.axiom-logback-appender]}))
 
 (defn jar [_]
       (compile-clj nil)
       (b/jar {:class-dir class-dir
-              :lib lib
-              :version version
-              :jar-file (format "target/%s-%s.jar" (name lib) version)}))
+              :lib       lib
+              :version   version
+              :jar-file  jar-file}))
 
 (defn install [_]
       (jar nil)
-      (b/install {:basis basis
-                  :lib lib
-                  :version version
-                  :jar-file jar-file
+      (b/install {:basis     basis
+                  :lib       lib
+                  :version   version
+                  :jar-file  jar-file
                   :class-dir class-dir}))
+
+(defn deploy [_]
+      (jar nil)
+      (dd/deploy {:installer :remote
+                  :artifact  jar-file
+                  :pom-file  (b/pom-path {:lib lib :class-dir class-dir})}))
